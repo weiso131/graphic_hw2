9682b51874234ad922271aa4455f73f1aa4ebc1f
許育瑋 <blackfire106.1231@gmail.com>
2025-05-03 17:46:34 +0900

    Adjust lp_array usage in morphing
    Previously, img1 used the reversed lp_array while
    img2 used the original one.
    However, this setup was unintuitive, as img1 is
    typically considered the source image.
    The order has now been swapped to reflect this expectation.

--- morphing/morphing.py
+++ morphing/morphing.py
@@ -8,7 +8,7 @@ def morphing(lp_array: list[LinePair], img_array1: np.ndarray, img_array2: np.nd
     for lp in lp_array:
         reverse_lp_array.append(lp.reverse())
         
-    new_img_array1 = mul_line_warping(img_array1, reverse_lp_array, alpha=alpha, a=a, b=b, p=p)
-    new_img_array2 = mul_line_warping(img_array2, lp_array, alpha=1 - alpha, a=a, b=b, p=p)
+    new_img_array1 = mul_line_warping(img_array1, lp_array, alpha=alpha, a=a, b=b, p=p)
+    new_img_array2 = mul_line_warping(img_array2, reverse_lp_array, alpha=1 - alpha, a=a, b=b, p=p)
 
     return new_img_array1, new_img_array2, new_img_array1 * (1 - alpha) + new_img_array2 * alpha
