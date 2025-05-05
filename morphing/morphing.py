16ce9db7cd9de4896377d3b9340a4cb2ddf8d277
許育瑋 <blackfire106.1231@gmail.com>
2025-05-05 15:57:31 +0900

    Use original image for specific alpha values
    When alpha is 0, the result is identical
    to the source image.
    When alpha is 1, the result is identical
    to the target image.

--- morphing/morphing.py
+++ morphing/morphing.py
@@ -1,14 +1,21 @@
 import numpy as np
-
+import math
 from .warping import LinePair, mul_line_warping
 
-def morphing(lp_array: list[LinePair], img_array1: np.ndarray, img_array2: np.ndarray, alpha=1, a=0.1, b=1, p=0.5):
+def morphing(lp_array: list[LinePair], img_array1: np.ndarray, img_array2: np.ndarray, alpha=1.0, a=0.1, b=1, p=0.5):
     reverse_lp_array = []
 
     for lp in lp_array:
         reverse_lp_array.append(lp.reverse())
-        
-    new_img_array1 = mul_line_warping(img_array1, lp_array, alpha=alpha, a=a, b=b, p=p)
-    new_img_array2 = mul_line_warping(img_array2, reverse_lp_array, alpha=1 - alpha, a=a, b=b, p=p)
+    
+    if math.isclose(alpha, 0.0):
+        new_img_array1 = img_array1
+    else:
+        new_img_array1 = mul_line_warping(img_array1, lp_array, alpha=alpha, a=a, b=b, p=p)
+
+    if math.isclose(alpha, 1.0):
+        new_img_array2 = img_array2
+    else:
+        new_img_array2 = mul_line_warping(img_array2, reverse_lp_array, alpha=1 - alpha, a=a, b=b, p=p)
 
     return new_img_array1, new_img_array2, new_img_array1 * (1 - alpha) + new_img_array2 * alpha
